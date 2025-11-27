ede7827b920cbe78dc5ca2d3c62f2c2d5a59aeb2
Satyam <sc10247@nyu.edu>
2025-11-26 19:49:22 -0500

    feat: Update model and config for llama 3.2 1B on T4 4x16 Baseten cloud

---
+++ new_demo/model/model.py
@@ -0,0 +1,47 @@
+"""
+The `Model` class is an interface between the ML model that you're packaging and the model
+server that you're running it on.
+
+The main methods to implement here are:
+* `load`: runs exactly once when the model server is spun up or patched and loads the
+   model onto the model server. Include any logic for initializing your model, such
+   as downloading model weights and loading the model into memory.
+* `predict`: runs every time the model server is called. Include any logic for model
+  inference and return the model output.
+
+See https://truss.baseten.co/quickstart for more.
+"""
+
+from transformers import pipeline
+
+
+
+class Model:
+    def __init__(self, **kwargs):
+        # Uncomment the following to get access
+        # to various parts of the Truss config.
+
+        # self._data_dir = kwargs["data_dir"]
+        # self._config = kwargs["config"]
+        self._secrets = kwargs["secrets"]
+        self._messages = [
+            {
+                "role": "user", 
+                "content": "You are an enthusiastic and helpful assistant. You are an elite and experienced recruiter and hiring manager in tech and help coach candidates land better roles and evaluate their fit with the job description."
+            }
+        ]
+        self._model = None
+
+    def load(self):
+        # Load model here and assign to self._model.
+        self._model = pipeline("text-generation", model="meta-llama/Llama-3.2-1B-Instruct",use_auth_token=self._secrets["hf_access_token"])
+
+    def predict(self, model_input):
+        # Run model inference here
+        model_input.append(
+            {
+                "role": "user",
+                "content": model_input
+            }
+        )
+        return self._model(model_input)
